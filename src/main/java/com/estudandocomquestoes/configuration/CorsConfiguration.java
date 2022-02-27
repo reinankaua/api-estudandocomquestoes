2f4de7831c87ef68a5d096e9f0c894510160b385
Reinan Kauã <contato.reinankaua@gmail.com>
2022-02-27 15:19:29 -0300

    New configuration

--- src/main/java/com/estudandocomquestoes/configuration/CorsConfiguration.java
+++ src/main/java/com/estudandocomquestoes/configuration/CorsConfiguration.java
@@ -10,12 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-
-        registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
