1a4107aa4d7e5858a625be65aebb5cf889985b19
Reinan Kauã <contato.reinankaua@gmail.com>
2022-03-25 12:36:42 -0300

    New Changes

---
+++ src/main/java/com/estudandocomquestoes/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.estudandocomquestoes.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+    }
+
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails build = User.withDefaultPasswordEncoder()
+                .username("reinankaua")
+                .password("123")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(build);
+    }
+}
