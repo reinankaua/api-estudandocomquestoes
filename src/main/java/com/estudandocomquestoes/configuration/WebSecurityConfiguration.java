e27233497b46d72f35caaf0b3e86e600540cbebb
Reinan Kauã <contato.reinankaua@gmail.com>
2022-03-26 14:27:49 -0300

    Oauth2

--- src/main/java/com/estudandocomquestoes/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/estudandocomquestoes/configuration/WebSecurityConfiguration.java
@@ -1,30 +1,38 @@
 package com.estudandocomquestoes.configuration;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@Configuration
 @EnableWebSecurity
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableAuthorizationServer
+@EnableResourceServer
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Bean
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("reinankaua").password("1234").roles("ADMIN");
     }
 
     @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails build = User.withDefaultPasswordEncoder()
-                .username("reinankaua")
-                .password("123")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(build);
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
